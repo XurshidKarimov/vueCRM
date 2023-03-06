1fb99d0bcf1fdcc3c053272bb79fcb81dc261606
Xurshid <kaxudi34801@gmail.com>
2023-03-07 01:54:23 +0500

    all-views done. templates removed

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,74 @@ const routes = [
   {
     path: "/",
     name: "home",
-    meta: { layout: 'main' },
+    meta: {
+      layout: 'main'
+    },
     component: () => import("@/views/HomeView.vue"),
   },
   {
     path: "/login",
     name: "login",
-    meta: { layout: 'empty' },
+    meta: {
+      layout: 'empty'
+    },
     component: () => import("@/views/LoginPage.vue"),
   },
   {
     path: "/categories",
     name: "categories",
-    meta: { layout: 'main' },
+    meta: {
+      layout: 'main'
+    },
     component: () => import("@/views/CategoryPage.vue"),
   },
   {
     path: '/register',
     name: 'register',
-    meta: {layout: 'empty'},
+    meta: {
+      layout: 'empty'
+    },
     component: () => import("@/views/RegisterPage.vue"),
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import("@/views/HistoryPage.vue"),
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import("@/views/DetailPage.vue"),
+  },
+  {
+    path: '/planing',
+    name: 'planing',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import("@/views/PlanningPage.vue"),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import("@/views/ProfilPage.vue"),
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import("@/views/RecordPage.vue"),
   }
 ];
 
